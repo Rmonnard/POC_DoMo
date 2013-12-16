59a57db636bd42e4bf93e0fa2f2d29ffb5db8db8
DragaoAzul45 <domigues.luis@gmail.com>
2013-12-16 13:21:40 +0100

    COnflict resolving

--- POC_Project/src/main/java/ch/heigvd/skeleton/jackson/JacksonConverter.java
+++ POC_Project/src/main/java/ch/heigvd/skeleton/jackson/JacksonConverter.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ch.heigvd.skeleton.jackson;
 
 import ch.heigvd.skeleton.model.Employee;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import java.io.IOException;
 import javax.ejb.Stateless;
 import org.codehaus.jackson.JsonParseException;
@@ -39,6 +39,31 @@ public class JacksonConverter {
         String json;
         try {
             json = ow.writeValueAsString(object);
+        } catch (IOException ex) {
+            json = "An error has occured. Look at your code.";
+        }
+        return json;
+    }
+
+    com.fasterxml.jackson.databind.ObjectMapper afterBurnerMapper = new com.fasterxml.jackson.databind.ObjectMapper();
+
+    public Object fromJsonAfterburner(String json) throws JsonParseException, JsonMappingException, IOException {
+        //com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper();
+        afterBurnerMapper.registerModule(new AfterburnerModule());
+        Employee empl = afterBurnerMapper.readValue(json, Employee.class);
+        return empl;
+    }
+
+    com.fasterxml.jackson.databind.ObjectWriter afterBurnerWriter;
+
+    public String toJSonAfterburner(Object object) {
+        //  com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper();
+        afterBurnerMapper.registerModule(new AfterburnerModule());
+        afterBurnerWriter = afterBurnerMapper.writer();
+        // ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json;
+        try {
+            json = afterBurnerWriter.writeValueAsString(object);
         } catch (IOException ex) {
             json = "An error has occured. Look at your code.";
         }
