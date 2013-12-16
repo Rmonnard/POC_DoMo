53d5fb1f84775dcd2b0cd187702b726fe0d85219
Matteo Tagliabue <matteotagliabue90@gmail.com>
2013-12-16 12:38:43 +0100

    Added Afterburner module (improved JSON serialization - deserialization)

--- POC_Project/src/main/java/ch/heigvd/skeleton/jackson/JacksonConverter.java
+++ POC_Project/src/main/java/ch/heigvd/skeleton/jackson/JacksonConverter.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ch.heigvd.skeleton.jackson;
 
 import ch.heigvd.skeleton.model.Employee;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import java.io.IOException;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -18,15 +18,36 @@ import org.codehaus.jackson.map.ObjectWriter;
  * @author luis
  */
 public class JacksonConverter {
-    
-    public Object fromJson(String json) throws JsonParseException
-                                                   , JsonMappingException, IOException{
-                Employee empl = new ObjectMapper().readValue(json, Employee.class);
-                return empl;
+
+    public Object fromJson(String json) throws JsonParseException, JsonMappingException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Employee empl = mapper.readValue(json, Employee.class);
+        return empl;
     }
-    
-    public String toJSon(Object object){
+
+    public String toJSon(Object object) {
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json;
+        try {
+            json = ow.writeValueAsString(object);
+        } catch (IOException ex) {
+            json = "An error has occured. Look at your code.";
+        }
+        return json;
+    }
+
+    public Object fromJsonAfterburner(String json) throws JsonParseException, JsonMappingException, IOException {
+        com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper();
+        mapper.registerModule(new AfterburnerModule());
+        Employee empl = mapper.readValue(json, Employee.class);
+        return empl;
+    }
+
+    public String toJSonAfterburner(Object object) {
+        com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper();
+        mapper.registerModule(new AfterburnerModule());
+        com.fasterxml.jackson.databind.ObjectWriter ow = mapper.writer();
+        // ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
         String json;
         try {
             json = ow.writeValueAsString(object);
---
+++ POC_Project/src/main/java/ch/heigvd/skeleton/servlets/PersonsServletAfterburner.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.skeleton.servlets;
+
+import ch.heigvd.skeleton.jackson.JacksonConverter;
+import ch.heigvd.skeleton.model.Employee;
+import ch.heigvd.skeleton.services.crud.EmployeesManagerLocal;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URI;
+import java.util.LinkedList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author luis
+ */
+@WebServlet(name = "PersonsServletAfterburner", urlPatterns = {"/PersonsServletAfterburner"})
+public class PersonsServletAfterburner extends HttpServlet {
+
+    @EJB
+    EmployeesManagerLocal employeesManager;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("This cannot append");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        List<Employee> employees = employeesManager.findAll();
+        try (PrintWriter out = response.getWriter()) {
+            String s = new JacksonConverter().toJSon(employees);
+            out.println(s);
+        }
+
+        //processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Employee newEmployee = new Employee();
+        String s = getBody(request);
+        JacksonConverter jc = new JacksonConverter();
+        Employee pe = (Employee) jc.fromJson(s);
+        //employeesTOService.updateEmployeeEntity(newEmployee, pe);
+        long newEmployeeId = employeesManager.create(pe);
+        response.setStatus(HttpServletResponse.SC_ACCEPTED);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    public static String getBody(HttpServletRequest request) throws IOException {
+
+        String body = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } catch (IOException ex) {
+            throw ex;
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException ex) {
+                    throw ex;
+                }
+            }
+        }
+
+        body = stringBuilder.toString();
+        return body;
+    }
+}
